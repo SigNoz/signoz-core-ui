4ca274b7727a73fe91e1d8a2a053e782a4218529
Palash <palashgdev@gmail.com>
2022-10-17 09:19:07 +0530

    chore: type issue is fixed

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,4 @@
 const peerDepsExternal = require("rollup-plugin-peer-deps-external");
-const resolve = require("@rollup/plugin-node-resolve");
 const commonjs = require("@rollup/plugin-commonjs");
 const typescript = require("rollup-plugin-typescript2");
 const postcss = require("rollup-plugin-postcss");
@@ -23,9 +22,8 @@ module.exports = {
   ],
   plugins: [
     peerDepsExternal(),
-    resolve(),
     commonjs(),
-    typescript({ useTsconfigDeclarationDir: true }),
+    typescript({}),
     postcss({
       extensions: [".css"],
     }),
