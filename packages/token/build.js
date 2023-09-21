cc9b6e7db27b4ee9d7c7eba7b36ccd7e411b4bad
Rajat-Dabade <rajat@signoz.io>
2023-09-21 20:47:18 +0530

    refactor: done with the initial stepup for theming

---
+++ packages/token/build.js
@@ -0,0 +1,75 @@
+const StyleDictionary = require('style-dictionary');
+const fs = require('fs-extra');
+const webPath = `build/dist/`;
+fs.removeSync(webPath);
+
+/**
+ * This function will wrap a built-in format and replace `.value` with `.darkValue`
+ * if a token has a `.darkValue`.
+ * @param {String} format - the name of the built-in format
+ * @returns {Function}
+ */
+function darkFormatWrapper(format) {
+  return function (args) {
+    const dictionary = Object.assign({}, args.dictionary);
+    dictionary.allProperties = dictionary.allProperties.map(token => {
+      const { darkValue } = token;
+      if (darkValue) {
+        const returnToken = Object.assign({}, token, {
+          value: token.darkValue
+        });
+        return returnToken;
+      } else {
+        return token;
+      }
+    });
+    dictionary.allTokens = dictionary.allTokens.map(token => {
+      const { darkValue } = token;
+      if (darkValue) {
+        const returnToken = Object.assign({}, token, {
+          value: token.darkValue
+        });
+        return returnToken;
+      } else {
+        return token;
+      }
+    });
+
+    args.dictionary = dictionary;
+    console.log(StyleDictionary.format[format]({ ...args })
+    );
+
+    // Use the built-in format but with our customized dictionary object
+    // so it will output the darkValue instead of the value
+    return StyleDictionary.format[format]({ ...args, dictionary })
+  }
+}
+
+StyleDictionary.extend({
+  // custom formats
+  format: {
+    scssDark: darkFormatWrapper(`scss/variables`),
+  },
+
+  source: [
+    `tokens/**/*.json`
+  ],
+
+  platforms: {
+    css: {
+      transformGroup: `scss`,
+      buildPath: webPath,
+      files: [{
+        destination: `variables.scss`,
+        format: `scss/variables`,
+        options: {
+          outputReferences: true
+        }
+      }, {
+        destination: `variables-dark.scss`,
+        format: `scssDark`,
+        filter: (token) => token.darkValue && token.attributes.category === `color`
+      }]
+    }
+  }
+}).buildAllPlatforms();
