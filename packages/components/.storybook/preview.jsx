a1303609b10d0ac234a138deeb5e1a17546a0128
Palash Gupta <palashgdev@gmail.com>
2023-12-14 15:43:01 +0530

    initial changes are updated

--- packages/components/.storybook/preview.jsx
+++ packages/components/.storybook/preview.jsx
@@ -1,1 +1,12 @@
+import { Theme } from "@radix-ui/themes";
+
 import "@radix-ui/themes/styles.css";
+import "../src/styles.scss";
+
+export const decorators = [
+  (Story) => (
+    <Theme>
+      <Story />
+    </Theme>
+  ),
+];
