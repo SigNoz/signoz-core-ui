7cb5667bab43398f751594ee5fcd284361bce136
Palash Gupta <palashgdev@gmail.com>
2023-12-17 19:38:13 +0530

    bumped version

--- packages/components/vite.config.ts
+++ packages/components/vite.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
     cssCodeSplit: true,
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
-      formats: ["es",'cjs'],
+      formats: ["es"],
     },
     rollupOptions: {
       external: ["react", "react/jsx-runtime"],
