07618dd9c9dd87b91f7758e4c4230854f92c4c37
Yunus M <myounis.ar@live.com>
2023-12-19 17:06:58 +0530

    feat: create tokens for colors, spacing and typography

--- packages/components/vite.config.ts
+++ packages/components/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@import "./node_modules/@ui-core/tokens/build/tokens/variables.scss"; @import "../src/_theme.scss";`,
+        additionalData: `@import "./node_modules/@ui-core/tokens/build/scss/_variables.scss"; @import "../src/_theme.scss";`,
       },
     },
   },
